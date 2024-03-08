bb14c1f7eab97f0be77c34a5874cd197131d56cc
Uglypr1nces <shpatavdiu4@gmail.com>
2024-03-08 11:19:00 +0100

    added a email sender thats not working, added some more functions to the robot

--- first_project/settings.py
+++ first_project/settings.py
@@ -18,7 +18,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEAMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEAMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
